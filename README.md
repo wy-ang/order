2bbbe8320cef559b318fe4b512438f48bcd8ba32
wangyang <wyang@yy.com>
2015-07-07 19:12:22 +0800

    1.优化数据本地存储 2.修改项目说明文档

--- README.md
+++ README.md
@@ -2,9 +2,3 @@
 #####order project
 ######Because often tangled what to eat at noon,So I wrote this little project.
 [view](http://htmlpreview.github.com/?https://github.com/wy-ang/order/blob/master/index.html)
-
-<pre>
-	<code>
-		<iframe src="https://d25lcipzij17d.cloudfront.net/badge.svg?id=gh&type=6&v=5.1.39&x2=0" frameborder="0"></iframe>
-	</code>
-</pre>
--- js/order.js
+++ js/order.js
@@ -1,39 +1,43 @@
-var liHtml,arr,newStrnewArr,iptVal;
+var liHtml,arr,newArr,newStrnewArr,iptVal;
 var confirm = $('.confirm');
+var ulDom = $('.order ul');
 var lastHtml = $('.order ul').children().last();
 var add = $('.add');
 var remove = $('.remove');
+var arr = [];
 /**
   *获取value值
   *拼接DOM
   */
 confirm.live('click',function(){
 	iptVal = $('.order input:text').val();
-	liHtml = '<li><span>'+iptVal+'</span><em class="add">添加</em><em class="remove">删除</em></li>';
-	localStorage.setItem("name",liHtml);
-	$('.addOrder').hide();
+	var oldVal = localStorage.getItem("key");
+	var newVal = oldVal+','+iptVal;
+	localStorage.setItem("key",newVal);
+	newArr = arr.push(newVal);
+	arrJoin = arr.join("");
+	var s = arrJoin.split(',');
+	for(var i = 0;i<s.length;i++){
+		liHtml = '<li><span>'+s[i]+'</span><em class="remove">删除</em></li>';
+	}
 	getArr();
+	$('.addOrder').hide();
 });
 /**
   *处理数据存储函数
   */
 function getArr(){
-	newStr = localStorage.getItem("name");
-	console.log(newStr);
-	$(newStr).insertBefore(lastHtml);  //TODO  此处不妥，有空试下for循环
-	console.log(lastHtml.html());
-	arr = ["测试数组1","测试数组2","测试数组3","测试数组4","测试数组5"];
-	newArr = arr.push(newStr);
-	var arrJoin = arr.join("");
-	 
+	$(liHtml).insertBefore(lastHtml);  //TODO  此处不妥，有空试下for循环，
 }
 getArr();
 /**
   *处理随机数
   */
 $('.submit').live('click',function(){
-	var randomN = Math.floor(Math.random() * newArr + 1)-1;
-	alert(arr[randomN]); 
+	if(newArr !== 0){
+		var randomN = Math.floor(Math.random() * newArr + 1)-1;
+		alert(arr[randomN]); 
+	}
 })
 /**
   *处理添加事件
@@ -47,4 +51,8 @@ add.live('click',function(){
 remove.live('click',function(){
 	$(this).parent().remove();
 	localStorage.clear();
+})
+
+ulDom.live('mous',function(){
+	add.show()
 })
