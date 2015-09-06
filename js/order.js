b7b05b1df18a8f011fb877e3cfff36bfed7874bf
wyang <wyang1117@msn.com>
2015-09-06 23:46:36 +0800

    1.页面美化 2.删除数据修改 3.修改页面数据存储方法

--- js/order.js
+++ js/order.js
@@ -4,20 +4,46 @@ var add = $('.add');
 var remove = $('.remove');
 
 /**
- * 添加Dom
- * @param 用户输入的数据
+ * @param opVal 用户输入的数据
+ * @param liId  数据列表标示
  */
-var addDom = function(opVal){
-	var liHtml = '<li><span>'+opVal+'</span><em class="remove">删除</em></li>';
+var addDom = function(opVal,liId){
+	var liHtml = '<li iptId='+liId+'><span>'+opVal+'</span><em class="remove">删除</em></li>';
 	$(liHtml).insertBefore($('.addOrder'));
 }
 
 /**
-  *处理数据存储
-  *处理数组分割
-  *拼接DOM
-  */
-var dataArr = function(){
+ *刷新页面数据存储
+ */ 
+var loadData = function(){
+	for(var i=0;i<localStorage.length;i++){
+		var loadArr = localStorage.getItem(localStorage.key(i));
+		var jsonObj = eval('('+loadArr+')');
+		addDom(jsonObj.name,jsonObj.iptId);
+		console.log(localStorage.key(i));
+	}
+}
+loadData();
+
+/**
+ * 回车提交事件
+ */
+$('input[type=text]').live('keydown',function(e){
+	if(e.which == 13){
+		confirmBtn.click();
+	}
+});
+
+/**
+ * 获取数据
+ * 存储数据
+ */
+confirmBtn.live('click',function(){
+	if($('input[type=text]').val()==''){
+		alert('请输入内容');
+		$('input[type=text]').val('').focus();
+		return false;
+	}
 	var iptVal = $('.order input:text').val();
 	addDom(iptVal);
 	var liIndex = ulDom.find('li').not('.addOrder,.last');
@@ -29,71 +55,32 @@ var dataArr = function(){
 		localStorage.setItem('name'+$index,'{name'+':'+$this.find('span').text()+',iptId'+':'+iptId+'}');
 	})
 	$('.addOrder').hide();
-}
-console.log($('input[type=hidden]').val());
-/**
- *处理刷新页面数据存储
- */ 
-var loadData = function(){
-	for(var i=0;i<localStorage.length;i++){
-		var loadArr = localStorage.getItem('name'+i);
-		var jsonObj = eval('('+loadArr+')');
-		addDom(jsonObj.name);
-	}
-}
-loadData();
-
-
-$('input[type=text]').keydown(function(e){
-	if(e.which == 13){
-		confirmBtn.click();
-	}
-})
-
-confirmBtn.live('click',function(){
-	if($('input[type=text]').val()==''){
-		alert('请输入内容');
-		$('input[type=text]').val('').focus();
-		return false;
-	}
-	dataArr();
 });
 
 /**
-  *处理添加事件
+  *添加事件
   */
 add.live('click',function(){
 	$('.addOrder').show();
 	$('input[type=text]').val('').focus();
-	add.hide();
 })
 
-
+/**
+ * 取消事件
+ */
 $('.cancel').live('click',function(){
 	$('.addOrder').hide();
 })
 
 /**
-  *处理删除事件
+  *删除事件
   */
 remove.live('click',function(){
-	var dataLength = localStorage.length;
-		console.log(dataLength);
 	var removeList = confirm('确定删除该项么？');
 	if(removeList){
 		$(this).parent().remove();
-		for(var i=0;i<dataLength;i++){
-			localStorage.removeItem('name'+i+'');
-		}
-
-	}
-})
-
-ulDom.live('mouseover mouseout',function(e){
-	if(e.type=='mouseover'){
-		add.show();
-	}else{
-		add.hide();
+		var parentId = $(this).parent().attr('iptId');
+		localStorage.removeItem('name'+parentId+'');
 	}
 })
 
